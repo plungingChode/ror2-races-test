375b750b783fe95b8f7ea31db41a30b63a6274d6
plungingChode <szigeti.peter23@gmail.com>
2022-10-03 09:16:56 +0200

    feat: add loadout model

---
+++ ExamplePlugin/Loadout.cs
@@ -0,0 +1,69 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamplePlugin;
+
+internal class Loadout {
+    public Loadout(uint[] loadout) {
+        this._skillVariants = loadout;
+    }
+
+    /// <summary>
+    /// Check if a <see cref="Survivor"/> has enough skill slots and variants for 
+    /// this loadout to be applied to them.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool IsValidForSurvivor(Survivor s) {
+        if (_skillVariants.Length != s.GetSkillSlotCount()) {
+            return false;
+        }
+        for (int i = 0; i < _skillVariants.Length; i++) {
+            if (_skillVariants[i] < 0 || _skillVariants[i] >= s.GetSkillVariantCount(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Set a loadout for a given <c>bodyIndex</c>.
+    /// </summary>
+    /// <param name="loadout"></param>
+    /// <param name="bodyIndex"></param>
+    public void Force(RoR2.Loadout loadout, BodyIndex bodyIndex) {
+        var blm = loadout.bodyLoadoutManager;
+
+        for (int i = 0; i < _skillVariants.Length; i++) {
+            blm.SetSkillVariant(bodyIndex, i, _skillVariants[i]);
+        }
+    }
+
+    /// <summary>
+    /// Print the internal names of this loadout.
+    /// </summary>
+    /// <param name="loadout"></param>
+    /// <param name="bodyIndex"></param>
+    /// <returns></returns>
+    public IEnumerable<string> Names(RoR2.Loadout loadout, BodyIndex bodyIndex) {
+        var names = new List<string>();
+        var blo = loadout.bodyLoadoutManager.GetReadOnlyBodyLoadout(bodyIndex);
+        
+        for (int i = 0; i < _skillVariants.Length; i++) {
+            var skill = blo.GetSkillFamily(i);
+            var variantIdx = (int)_skillVariants[i];
+            names.Add(skill.GetVariantName(variantIdx));
+        }
+
+        return names;
+    }
+
+    /// <summary>
+    /// The internal representation of the loadout. The number of elements must
+    /// match the number of skills for a given <see cref="BodyIndex"/>, and the
+    /// elements themselves are the variant indeces.
+    /// </summary>
+    private readonly uint[] _skillVariants;
+}
--- ExamplePlugin/State.cs
+++ ExamplePlugin/State.cs
@@ -1,31 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
+using System.Linq;
+using RoR2;
+using UnityEngine.Assertions;
 
-namespace ExamplePlugin {
-    // TODO could also use BepInEx's Config manager thing, but maybe that should
-    //      only be used for actual configuration (like the server URL)
-    internal class State {
-        private string? _playerName;
-        public string PlayerName => _playerName ?? "";
-        public bool IsPlayedSignedIn => _playerName != null;
+namespace ExamplePlugin;
 
-        public RaceSetup? race;
+// TODO could also use BepInEx's Config manager thing, but maybe that should
+//      only be used for actual configuration (like the server URL)
 
-        public void SignIn(string playerName) {
-            _playerName = playerName;
-        }
+/// <summary>
+/// Represents the internal state of the mod.
+/// </summary>
+internal class State {
+    public static readonly State instance = new();
 
-        public void SignOut() {
-            _playerName = null;
-        }
+    public string PlayerName => _playerName ?? "";
+    public bool IsSignedIn => _playerName != null;
+
+    // TODO
+    public RaceTiming? raceTiming;
+    public RaceLoadout? raceLoadout;
+
+    public bool IsClockedIn => true;
+    public bool IsRacing => IsSignedIn && IsClockedIn && raceTiming != null;
+
+    public void SignIn(string playerName) {
+        _playerName = playerName;
     }
 
-    struct RaceSetup {
-        public DateTime localStart;
-        public DateTime localEnd;
-        public long timeInc;
-        public string overall;
+    public void SignOut() {
+        _playerName = null;
     }
+
+    private State() { }
+    private string? _playerName;
+
+    // TODO add save/load
+    // public string Serialize()
+    // public static State Deserialize()
+}
+
+/// <summary>
+/// Describes a race's timing.
+/// </summary>
+struct RaceTiming {
+    public DateTime localStart;
+    public DateTime localEnd;
+    public long timeInc;
+    public string overall;
+}
+
+/// <summary>
+/// Contains a race's loadouts.
+/// </summary>
+struct RaceLoadout {
+    public RaceLoadout(IEnumerable<Loadout> loadouts) {
+        Assert.IsTrue(loadouts.Any());
+
+        _loadouts = new List<Loadout>(loadouts);
+        _currentLoadoutIdx = 0;
+    }
+
+    public Loadout CurrentLoadout => _loadouts[_currentLoadoutIdx];
+    public int RemainingLoadouts => Math.Max(_loadouts.Count - 1, 0);
+
+    /// <summary>
+    /// Discard the current loadout and set the one at <paramref name="newIdx"/>
+    /// as the current.
+    /// </summary>
+    public void RotateCurrentLoadout(int newIdx) {
+        Assert.IsTrue(RemainingLoadouts > 0);
+        Assert.IsTrue(_loadouts.Count > newIdx);
+
+        _loadouts.RemoveAt(_currentLoadoutIdx);
+        _currentLoadoutIdx = newIdx;
+    }
+
+    private List<Loadout> _loadouts;
+    private int _currentLoadoutIdx;
 }
